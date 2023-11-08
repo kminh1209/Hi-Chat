f957ca48ffb4ca444244e403b609ec33ba7d47d1
Kang MinHyeok <kminh1209@naver.com>
2023-11-08 20:44:36 +0900

    ver 1.1.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,17 +10,38 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import json
+import os
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+#fg4e0&i@)a(&!j1@^f&9hpx#(1(%%_mr(_22__2uiisoapb'
+# SECRET_KEY = 'django-insecure-k+#fg4e0&i@)a(&!j1@^f&9hpx#(1(%%_mr(_22__2uiisoapb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+#from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Choice, Question
+from .models import Question
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -19,39 +20,42 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
-class ResultsView(generic.DetailView):
+
+
+class ResultsView(generic.DetailView):      #결과를 위한 class
     model = Question
     template_name = "polls/results.html"
 
-def vote(request, question_id):
-    if request.method =='GET':
-        pass
-    elif request.method == 'POST':
-        question = get_object_or_404(Question, pk=question_id)
-        try:
-            selected_choice = question.choice_set.get(pk=request.POST["choice"])
-        except (KeyError, Choice.DoesNotExist):
-            # Redisplay the question voting form.
-            return render(
-                request,
-                "polls/detail.html",
-                {
-                    "question": question,
-                    "error_message": "You didn't select a choice.",
-                },
-            )
-        else:
-            selected_choice.votes += 1
-            selected_choice.save()
-            # Always return an HttpResponseRedirect after successfully dealing
-            # with POST data. This prevents data from being posted twice if a
-            # user hits the Back button.
-            return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+# def vote(request, question_id):
+#     if request.method =='GET':
+#         pass
+#     elif request.method == 'POST':
+#         question = get_object_or_404(Question, pk=question_id)
+#         try:
+#             selected_choice = question.choice_set.get(pk=request.POST["choice"])
+#         except (KeyError, Choice.DoesNotExist):
+#             # Redisplay the question voting form.
+#             return render(
+#                 request,
+#                 "polls/detail.html",
+#                 {
+#                     "question": question,
+#                     "error_message": "You didn't select a choice.",
+#                 },
+#             )
+#         else:
+#             selected_choice.votes += 1
+#             selected_choice.save()
+#             # Always return an HttpResponseRedirect after successfully dealing
+#             # with POST data. This prevents data from being posted twice if a
+#             # user hits the Back button.
+#             return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
