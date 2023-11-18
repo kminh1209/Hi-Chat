06755c3ddcb7c11db85c7c088c405c0d0f617bba
Paul Kang <kminh1209@naver.com>
2023-11-18 22:52:43 +0900

    ver 1.1.3
    코드 주석 추가

--- GPT/views.py
+++ GPT/views.py
@@ -1,13 +1,19 @@
+# 데이터베이스에서 객체를 가져오거나, 존재하지 않을 경우 404 에러를 반환
 from django.shortcuts import get_object_or_404, get_list_or_404
+# api_view 데코레이터를 import, 데코레이터:함수 기반의 뷰를 API 뷰로 변환해줌
 from rest_framework.decorators import api_view
+# API 응답을 생성하는데 사용됨
 from rest_framework.response import Response
+# HTTP 상태 코드를 제공
 from rest_framework import status
+# Django 프로젝트의 설정을 관리하는데 사용됨
 from django.conf import settings
 import requests, json
 
-API_KEY = settings.API_KEY
+API_KEY = settings.API_KEY #외부 API에 대한 인증
 
-@api_view(['GET'])
+# @api_view 데코레이터를 적용하여 GET 메서드만 허용하는 API 뷰로 설정
+@api_view(['GET'])  
 def products(request):
     url = 'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json'
     params = {
