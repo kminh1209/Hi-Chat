f46746bfe6132f1dc81e1c1d9e536e774d112257
Kang MinHyeok <kminh1209@naver.com>
2023-11-19 17:08:41 +0900

    ver1.3
    Question 템플릿 수정

---
+++ Question/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'Question'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.answer, name='answer'),
+]
+
