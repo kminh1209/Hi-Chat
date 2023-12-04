63ededd6240736cd5be0720c51e1f075ecd77d8a
Kang MinHyeok <kminh1209@naver.com>
2023-12-04 11:33:56 +0900

    ver 1.5.1
    - Question app
    - q_food app
    
    DB setting complete

--- Question/urls.py
+++ Question/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'Question'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.answer, name='answer'),
+    # path('<int:question_id>/', views.answer, name='answer'),
 ]
 
--- Question/views.py
+++ Question/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Answer
 
-def index(request):
+# def index(request):
+#     questions = Question.objects.all()
+#     return render(request, 'q_food/index.html', {'questions': questions})
+
+# def answer(request, question_id): # 각각의 질문에 대한 답변을 처리하는 구조
+#     question = get_object_or_404(Question, pk=question_id)
+    
+#     if request.method == 'POST':
+#         answer_text = request.POST.get('answer_text', '')
+#         Answer.objects.create(question=question, answer_text=answer_text)
+#         return redirect('q_food:detail', question_id=question.id)
+
+#     return render(request, 'q_food/answer.html', {'question': question})
+
+def index(request): # 여러 질문에 대한 답변을 한 번에 입력받아 처리하고자 하는 경우
     questions = Question.objects.all()
+
+    if request.method == 'POST':
+        for question in questions:
+            answer_text = request.POST.get(f'answer_{question.id}', '')
+            Answer.objects.create(question=question, answer_text=answer_text)
+
     return render(request, 'Question/index.html', {'questions': questions})
 
-def answer(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.method == 'POST':
-        answer_text = request.POST['answer_text']
-        Answer.objects.create(question=question, answer_text=answer_text)
-        return HttpResponseRedirect(reverse('Question:index'))
-    return render(request, 'Question/answer.html', {'question': question})
