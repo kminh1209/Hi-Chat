9a770773b19d8bcf58b42cd3bfb6ffca652a395e
Kang MinHyeok <kminh1209@naver.com>
2023-12-04 11:19:24 +0900

    ver 1.5.0
    DB 연결 완료
    - 질문에 대한 답변을 볼 수 있음

--- q_food/urls.py
+++ q_food/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'q_food'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.answer, name='answer'),
+    #path('<int:question_id>/', views.answer, name='answer'),
 ]
 
